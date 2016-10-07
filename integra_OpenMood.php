2ea234dd1b5bd412a18c2e1bb914c170e630b094
Marcelo Caetano <marcelo.caetano@assistemas.com.br>
2016-10-07 18:06:39 -0300

    perfect

--- integra_OpenMood.php
+++ integra_OpenMood.php
@@ -39,75 +39,88 @@ while ($row = $query_view->fetch_object())
     $sql_linha = $con->query("SELECT email FROM moodle.mdl_user WHERE email='$email'") or die($mysqli->errno .' - '. $mysqli->error);
     $sql_linha_result = $sql_linha->num_rows;
 
-    if ($sql_linha_result === 0)
+        // VERIFICA SE O ALUNO ESTA MATRICULADO NO CURSO
+
+        $verifica_inscricao = $con->query("SELECT me.courseid, mue.userid FROM mdl_user_enrolments mue
+                                            INNER JOIN mdl_user mu ON mu.id = mue.userid
+                                            INNER JOIN mdl_enrol me ON mue.enrolid = me.id
+                                            WHERE me.courseid ='$courseid' AND mue.userid ='$idUsuarioMoodle' AND mu.email='$email'");
+
+        $result_verifica_inscricao = $verifica_inscricao->num_rows;
+        
+
+    if ($result_verifica_inscricao === 0)
     {
-        echo "<br>E-mail não existe. ".$email."<br /><br />";
 
+       
+            echo "<br>E-mail não existe. ".$email."<br /><br />";
+
+            
+            // SE NAO ESTIVER, CADASTRA O ALUNO
+            // // Registra Aluno Novo
+            cadastraAluno($username,$password,$firstname,$lastname,$email,$description);
+
+            $last_idUser = $con->insert_id;
+
+
+            // SE O CADASTRO FOR BEM SUCEDIDO, CADASTRA E EFETUA MATRICULA DO ALUNO NO CURSO
+            // Recupera a chave da modalidade da matrícula do curso
+            $result_courseid = $con->query("SELECT id FROM moodle.mdl_enrol WHERE courseid = '$courseid' AND enrol='manual'");
+            $row = $result_courseid->fetch_assoc();
+            $mdl_enrol_id = $row['id'];
+
+            cadastraAlunoCurso($mdl_enrol_id,$last_idUser,$timestamp_datainicio,$timestamp_datafinal,$timestamp_datainicio,$timestamp_datainicio);
+
+            // Recupera o contexto do curso.
+            $result_contextCurso = $con->query("SELECT id FROM moodle.mdl_context WHERE instanceid = '$courseid' AND contextlevel=50");
+            $row_context = $result_contextCurso->fetch_assoc();
+            $result_contexid = $row_context['id'];        
+
+            efetuaMatriculaAluno($result_contexid,$last_idUser,$timestamp_datainicio);
+            
+            // ENVIA EMAIL PARA O ALUNO E O COORDENADOR
+
+            $clone_email = clone $mail; 
+            
+            $clone_email->addAddress($email); 
+            $clone_email->Subject = 'Olá '.$firstname.', aqui está sua conta do Aulas a Distância'; 
+            $clone_email->Body    = '<p><img src="https://aulasadistancia.com.br/site/templates/ol_chranet/images/logo/logoEAD.png" border="0"></p> 
+            '.cumprimento().' '.$firstname.', <br /><br /> 
+
+            &Eacute; com muita satisfa&ccedil;&atilde;o que informamos que voc&ecirc; foi cadastrado(a) na plataforma de cursos do Aulas a Dist&acirc;ncia, logo a baixo est&atilde;o os dados para o seu acesso.<br><br> 
+
+            ----------------------------------<br /><br /> 
+            Curso: <strong>'.$nomecurso.'<br /> 
+            </strong>Prazo: <strong>'.$datainicio.'</strong>&nbsp;a&nbsp;<strong>'.$datafim.'<br /> 
+            </strong>URL: <a title="Aulas a Dist&acirc;ncia" href="https://aulasadistancia.com.br/site/" target="_blank">https://aulasadistancia.com.br/site/</a> <br /> 
+            clique em <em>"&Aacute;rea do Aluno"</em> e peencha com os dados a seguir<br /><br /> 
+            Dados de acesso.<br /> 
+            e-Mail: <strong>'.$email.'<br /> 
+            </strong>Senha: <strong>'.$passuser.'</strong> <br /><br /> 
+            ----------------------------------<br /><br /> 
+
+            - A impress&atilde;o do certificado e o download dos materiais do curso devem ser realizados dentro do prazo acima.<br /> 
+            - A gera&ccedil;&atilde;o do certificado fora do prazo adquirido somente acontecer&aacute; mediante pagamento de uma taxa.<br /> 
+            - Para altera&ccedil;&otilde;es do seu nome entre no curso com os dados informados acima e localize a se&ccedil;&atilde;o Administra&ccedil;&atilde;o.<br /> 
+            - Clique em <em>"Minhas configura&ccedil;&otilde;es de perfil"</em> e em seguida clique em <em>"Modificar Perfil"</em>.<br /> 
+            - Fa&ccedil;a as altera&ccedil;&otilde;es desejadas e clique no bot&atilde;o <em>"Atualizar perfil"</em> no fim da p&aacute;gina.<br /><br /> 
+
+            Qualquer d&uacute;vida entre em contato com o suporte via chat online na tela inicial da plataforma ou pelo e-mail suporte@dietpro.com.br <br /><br /> 
+            Bons estudos!!!<br/><br/> 
+            Atenciosamente.<br /><br />
+            Alan Pereira<br/>
+            relaciomanento com Cliente<br/>
+            relacionamento.cliente@assistemas.com.br<br/>
+            (31) 3891-9898'; 
+
+            if(!$clone_email->send())
+            {
+                echo 'Mailer Error: ' . $clone_email->ErrorInfo;
+                exit;
+            } 
+            echo 'Message has been sent <br>';
         
-        // SE NAO ESTIVER, CADASTRA O ALUNO
-        // // Registra Aluno Novo
-        cadastraAluno($username,$password,$firstname,$lastname,$email,$description);
 
-        $last_idUser = $con->insert_id;
-
-
-        // SE O CADASTRO FOR BEM SUCEDIDO, CADASTRA E EFETUA MATRICULA DO ALUNO NO CURSO
-        // Recupera a chave da modalidade da matrícula do curso
-        $result_courseid = $con->query("SELECT id FROM moodle.mdl_enrol WHERE courseid = '$courseid' AND enrol='manual'");
-        $row = $result_courseid->fetch_assoc();
-        $mdl_enrol_id = $row['id'];
-
-        cadastraAlunoCurso($mdl_enrol_id,$last_idUser,$timestamp_datainicio,$timestamp_datafinal,$timestamp_datainicio,$timestamp_datainicio);
-
-        // Recupera o contexto do curso.
-        $result_contextCurso = $con->query("SELECT id FROM moodle.mdl_context WHERE instanceid = '$courseid' AND contextlevel=50");
-        $row_context = $result_contextCurso->fetch_assoc();
-        $result_contexid = $row_context['id'];        
-
-        efetuaMatriculaAluno($result_contexid,$last_idUser,$timestamp_datainicio);
-        
-        // ENVIA EMAIL PARA O ALUNO E O COORDENADOR
-
-        $clone_email = clone $mail; 
-        
-        $clone_email->addAddress($email); 
-        $clone_email->Subject = 'Olá '.$firstname.', aqui está sua conta do Aulas a Distância'; 
-        $clone_email->Body    = '<p><img src="https://aulasadistancia.com.br/site/templates/ol_chranet/images/logo/logoEAD.png" border="0"></p> 
-        '.cumprimento().' '.$firstname.', <br /><br /> 
-
-        &Eacute; com muita satisfa&ccedil;&atilde;o que informamos que voc&ecirc; foi cadastrado(a) na plataforma de cursos do Aulas a Dist&acirc;ncia, logo a baixo est&atilde;o os dados para o seu acesso.<br><br> 
-
-        ----------------------------------<br /><br /> 
-        Curso: <strong>'.$nomecurso.'<br /> 
-        </strong>Prazo: <strong>'.$datainicio.'</strong>&nbsp;a&nbsp;<strong>'.$datafim.'<br /> 
-        </strong>URL: <a title="Aulas a Dist&acirc;ncia" href="https://aulasadistancia.com.br/site/" target="_blank">https://aulasadistancia.com.br/site/</a> <br /> 
-        clique em <em>"&Aacute;rea do Aluno"</em> e peencha com os dados a seguir<br /><br /> 
-        Dados de acesso.<br /> 
-        e-Mail: <strong>'.$email.'<br /> 
-        </strong>Senha: <strong>'.$passuser.'</strong> <br /><br /> 
-        ----------------------------------<br /><br /> 
-
-        - A impress&atilde;o do certificado e o download dos materiais do curso devem ser realizados dentro do prazo acima.<br /> 
-        - A gera&ccedil;&atilde;o do certificado fora do prazo adquirido somente acontecer&aacute; mediante pagamento de uma taxa.<br /> 
-        - Para altera&ccedil;&otilde;es do seu nome entre no curso com os dados informados acima e localize a se&ccedil;&atilde;o Administra&ccedil;&atilde;o.<br /> 
-        - Clique em <em>"Minhas configura&ccedil;&otilde;es de perfil"</em> e em seguida clique em <em>"Modificar Perfil"</em>.<br /> 
-        - Fa&ccedil;a as altera&ccedil;&otilde;es desejadas e clique no bot&atilde;o <em>"Atualizar perfil"</em> no fim da p&aacute;gina.<br /><br /> 
-
-        Qualquer d&uacute;vida entre em contato com o suporte via chat online na tela inicial da plataforma ou pelo e-mail suporte@dietpro.com.br <br /><br /> 
-        Bons estudos!!!<br/><br/> 
-        Atenciosamente.<br /><br />
-        Alan Pereira<br/>
-        relaciomanento com Cliente<br/>
-        relacionamento.cliente@assistemas.com.br<br/>
-        (31) 3891-9898'; 
-
-        if(!$clone_email->send())
-        {
-            echo 'Mailer Error: ' . $clone_email->ErrorInfo;
-            exit;
-        } 
-        echo 'Message has been sent <br>';
-        
     }
     else // SE O ALUNO JA ESTIVER MATRICULADO MAS NAO ESTIVER EM NENHUM CURSO, FAZ A VERIFICAÇÃO POR CURSO E PERÍODO VIGENTE
     {
